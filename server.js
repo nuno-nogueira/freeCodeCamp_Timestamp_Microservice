9f7e695ad91e32efc6769302e64ee9a595c866dc
Nuno Nogueira <nunonogueira05@gmail.com>
2025-11-10 11:41:15 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const bGround = require('fcc-express-bground');
+const app = require('./app');
+const express = require('express');
+const app = express();
+
+if (!process.env.DISABLE_XORIGIN) {
+  app.use((req, res, next) => {
+    const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    const origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
+
+const port = process.env.PORT || 3000;
+bGround.setupBackgroundApp(app, app, __dirname).listen(port, () => {
+  bGround.log(`Node is listening on port ${port}...`);
+});
